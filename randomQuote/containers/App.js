b482fab1cbb12957d675a0c51412da73901da9a5
Tobi <Tobi@LAUSDMacD2F3F7.frontierlocal.net>
2019-03-31 15:48:09 -0700

    adapted React comp to single source of truth

--- randomQuote/containers/App.js
+++ randomQuote/containers/App.js
@@ -1,5 +1,65 @@
 import React, { Component } from 'react';
 
+const database = [
+  { name: 'Vigeneareay', value: 'she is so beautiful' },
+  { name: 'Wing', value: 'she is amazing' },
+  { name: 'Vidget', value: 'she is lovely' }
+];
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    let index = Math.floor(Math.random() * database.length);
+    this.state = {
+      person: database[index].name,
+      quote: database[index].value
+    };
+
+    this.updateQuote = this.updateQuote.bind(this);
+  }
+
+  updateQuote(x, y) {
+    this.setState({
+      person: x,
+      quote: y
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Quote:</h1>
+        <DisplayQuote person={this.state.person} quote={this.state.quote} />
+        <a id="tweet-quote" href='twitter.com/intent/tweet"'>
+          <i className="fa fa-twitter" />
+        </a>
+        <PickQuote database={database} updateQuote={this.updateQuote} />
+      </div>
+    );
+  }
+}
+
+class PickQuote extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeQuote = this.changeQuote.bind(this);
+    this.getNum = this.getNum.bind(this);
+  }
+  changeQuote() {
+    console.log('change?');
+    this.val = this.getNum();
+    this.props.updateQuote(database[this.val].name, database[this.val].value);
+  }
+  getNum() {
+    return Math.floor(Math.random() * database.length);
+  }
+  render() {
+    return (
+      <button onClick={this.changeQuote} id="new-quote">
+        Next
+      </button>
+    );
+  }
+}
+
 const DisplayQuote = props => {
   return (
     <div>
@@ -8,58 +68,3 @@ const DisplayQuote = props => {
     </div>
   );
 };
-
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <h1>Quote:</h1>
-        <GenerateQuote />
-      </div>
-    );
-  }
-}
-
-class GenerateQuote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.database = [
-      { name: 'Vigeneareay', value: 'she is so beautiful' },
-      { name: 'Wing', value: 'she is amazing' },
-      { name: 'Vidget', value: 'she is lovely' }
-    ];
-    this.val = this.getNum();
-    this.state = {
-      person: this.database[this.val].name,
-      quote: this.database[this.val].value
-    };
-    this.changeQuote = this.changeQuote.bind(this);
-    this.getNum = this.getNum.bind(this);
-  }
-  changeQuote() {
-    this.val = this.getNum();
-    this.setState({
-      person: this.database[this.val].name,
-      quote: this.database[this.val].value
-    });
-  }
-  getNum() {
-    return Math.floor(Math.random() * this.database.length);
-  }
-  render() {
-    return (
-      <div>
-        <DisplayQuote person={this.state.person} quote={this.state.quote} />
-        <a id="tweet-quote" href='twitter.com/intent/tweet"'>
-          <i class="fa fa-twitter-square" />
-        </a>
-        <button onClick={this.changeQuote} id="new-quote">
-          Next
-        </button>
-      </div>
-    );
-  }
-}
