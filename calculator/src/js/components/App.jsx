a2546061da15570ee2787edc5b59bac32cec8967
Tobi <Tobi@LAUSDMacD2F3F7.frontierlocal.net>
2019-05-11 22:39:44 -0700

    created calculator

---
+++ calculator/src/js/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReduxCalculator from './Calculator.jsx';
+
+const App = () => (
+  <div>
+    <ReduxCalculator />
+  </div>
+);
+export default App;
---
+++ calculator/webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWPPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWPPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+};
