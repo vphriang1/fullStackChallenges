7a19dce1120686a188f3db3cdee2a4ab69d37678
user <user@lausds-MacBook.local>
2021-03-26 11:58:10 -0700

    working multer

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,6 +13,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+  res.json({"name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size});
+});
 
 
 
